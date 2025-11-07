8195c4ca178fdf933c61fad5210e66821c564a35
EduardKhismatov <E.Khismatov@yandex.ru>
2025-11-07 23:29:36 +0600

    for

--- src/loops.js
+++ src/loops.js
@@ -1,4 +1,5 @@
 import { generateInteger } from './numbers.js';
+import chalk from 'chalk';
 
 //Подсчет
 export const numbers = (arr = []) => {
@@ -63,4 +64,181 @@ export const revert = (arr) => {
     reverseArr.push(arr[i]);
   }
   return reverseArr.join('');
+};
+
+//Подходящие люди
+export const filter = (people) => {
+  const peopleFilter = {
+    mans: [],
+    women: [],
+  };
+  for (const peopleElement of people) {
+    if (peopleElement.gender === 'f' && peopleElement.age > 20) {
+      peopleFilter.women.push(peopleElement.name);
+    }
+    if (peopleElement.gender === 'm' && peopleElement.age > 18) {
+      peopleFilter.mans.push(peopleElement.name);
+    }
+  }
+  return peopleFilter;
+};
+
+//Округлить значения
+export const rounding = (arr) => {
+  let sumPositive = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      sumPositive += 1;
+    }
+  }
+  sumPositive -= 3;
+  let count = 0;
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0 && count < sumPositive) {
+      newArr.push(Math.round(arr[i]));
+      count += 1;
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+//Ёлочка
+export const christmas = (num) => {
+  let itog = '';
+  for (let i = 1; i <= num; i++) {
+    itog += `${' '.repeat(num - i)} ${'*'.repeat(i)}${'*'.repeat(i - 1)}\n`;
+  }
+  return itog;
+};
+
+//Цветной вывод чисел
+export const colorNumber = (num) => {
+  for (let i = 0; i < num; i++) {
+    const numberItog = i % 3;
+    if (numberItog === 2) {
+      console.log(chalk.green(i));
+    } else if (numberItog === 1) {
+      console.log(chalk.red(i));
+    } else {
+      console.log(chalk.red(i));
+    }
+  }
+};
+
+//Цветной вывод ключей - значений
+export const printColorObject = (num) => {
+  for (const numElement in num) {
+    console.log(`${chalk.red(numElement)}: ${chalk.yellow(num[numElement])} `);
+  }
+};
+
+//Произведение
+export const findMultiply = (num1, num2) => {
+  let numberFinal = '';
+  for (let i = 0; i < String(num1).length; i++) {
+    for (let j = 1; j < String(num1).length; j++) {
+      if (Number(String(num1)[i]) * Number(String(num1)[j]) === num2 && i !== j) {
+        numberFinal += String(i) + String(j);
+        return numberFinal;
+      }
+    }
+  }
+  if (numberFinal.length === 0) {
+    return '-1-1';
+  }
+};
+
+//Вывод матрицы. ВЛОЖЕННЫМ ЦИКЛОМ НЕПОЛУЧИЛОСЬ, ПОЛУЧИЛОСЬ ТОЛЬО ТАК
+export const matrix = (mat) => {
+  for (let i = 0; i < mat.length; i++) {
+    console.log(`${mat[i].join('\t')}\n`);
+  }
+};
+
+//Возраст
+export const func = (userAge) => {
+  const objAge = [
+    { 'Возраст=': 25, 'Количество людей=': 0 },
+    { 'Возраст=': 28, 'Количество людей=': 0 },
+    { 'Возраст=': 30, 'Количество людей=': 0 },
+    { 'Возраст=': 35, 'Количество людей=': 0 },
+    { 'Возраст=': 41, 'Количество людей=': 0 },
+  ];
+  for (let i = 0; i < userAge.length; i++) {
+    for (let j = 0; j < objAge.length; j++) {
+      if (userAge[i].age === objAge[j]['Возраст=']) {
+        objAge[j]['Количество людей='] += 1;
+      }
+    }
+    // console.log(userAge[i].name);
+  }
+  return objAge;
+};
+
+//Самый длинный массив
+export const lengthArray = (arr) => {
+  let array = [];
+  for (const key in arr) {
+    if (Array.isArray(arr[key]) && arr[key].length > array.length) {
+      array = arr[key];
+    }
+  }
+  return { malen: array.length, lastValue: array[array.length - 1] };
+};
+
+//Сравнение массивов
+export const compareArrays = (a, b) => {
+  if (a.length !== b.length) return false;
+  else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+//Сравнение объектов
+export const compareObjects = (object1, object2) => {
+  const objKey1 = Object.keys(object1);
+  const objKey2 = Object.keys(object2);
+  let counter = 0;
+  let counter2 = 0;
+  if (objKey1.length !== objKey1.length) return false;
+  else {
+    for (let i = 0; i < objKey1.length; i++) {
+      if (objKey1.includes(objKey2[i])) {
+        counter += 1;
+      }
+    }
+  }
+  const objValue1 = Object.values(object1);
+  const objValue2 = Object.values(object2);
+  if (objValue1.length !== objValue2.length) return false;
+  else {
+    for (let i = 0; i < objValue1.length; i++) {
+      if (objValue1.includes(objValue2[i])) {
+        counter2 += 1;
+      }
+    }
+  }
+  return counter2 === counter;
+};
+
+//Сравнение массивов объектов
+export const compareObjectArrays = (a, b) => {
+  let flag = 0;
+  if (a.length !== b.length) return false;
+  else {
+    for (let i = 0; i < a.length; i++) {
+      if (compareObjects(a[i], b[i])) {
+        flag += 1;
+      }
+    }
+  }
+  return flag === a.length;
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import chalk from 'chalk';
 import { sayHi, sayText, sum, power, tomatoPrice } from './functions.js';
 import { lengthWord, letter, isStringStartsWith, people, logStringInfo } from './strings.js';
-import { square, radius, isEven, cashRegister, dimensions, schoolDesks, temperature } from './numbers.js';
+import {
+  square,
+  radius,
+  isEven,
+  cashRegister,
+  dimensions,
+  schoolDesks,
+  temperature,
+  generateInteger,
+} from './numbers.js';
 import { compare, compareStrings } from './compares.js';
 import { age, FizzBuzz, randomYear } from './if.js';
 import { character, processing } from './switches.js';
@@ -9,7 +18,24 @@ import { addProperty, removeProperty, compares, relocation, agePerson } from './
 import { getRandomItem, logJoin, setNickname, add, orderFood } from './arrays.js';
 import { mergeToNewObject, merge, report, showInfo } from './spread.js';
 import { comparison } from './type.js';
-import { averages, double, numbers, revert } from './loops.js';
+import {
+  averages,
+  christmas,
+  colorNumber,
+  double,
+  filter,
+  numbers,
+  revert,
+  rounding,
+  printColorObject,
+  findMultiply,
+  matrix,
+  func,
+  lengthArray,
+  compareArrays,
+  compareObjects,
+  compareObjectArrays,
+} from './loops.js';
 
 const color = new Date().toISOString();
 
@@ -90,7 +116,7 @@ const color = new Date().toISOString();
 // console.log(processing('Клиент отказался от заказа'));
 // console.log(processing('Заказ доставлен'));
 // console.log(processing('Что то еще'));
-
+//
 // console.log(removeProperty({ id: 1, age: 20, name: 'alex' }, 'id'));
 // console.log(addProperty({ id: 5, name: 'max' }, 'address', 'ул. Углеродная 5'));
 //
@@ -129,7 +155,7 @@ const color = new Date().toISOString();
 // console.log(chalk.green(agePerson(jake)));
 // console.log(chalk.grey(agePerson(john)));
 // console.log(chalk.red(agePerson(bob)));
-
+//
 // console.log(logJoin(['max', 'john', 123, null], '_')); // max_john_123_
 // console.log(logJoin([1, 2, 3], '-')); // 1-2-3
 //
@@ -138,7 +164,7 @@ const color = new Date().toISOString();
 // console.log(setNickname(user, 'Саша'));
 //
 // console.log(getRandomItem([1, 2, 3, 4, 5, 6, 7, 8]));
-
+//
 // const list = [];
 //
 // add(list, 'Картошка');
@@ -157,23 +183,23 @@ const color = new Date().toISOString();
 // add(list2, 3);
 // add(list2, 2);
 // console.log(list2);
-
+//
 // const cheque = {
 //   food: ['Сырный суп', 'Бургер', 'Лазанья'],
 //   drinks: ['Пиво', 'Молочный коктейль'],
 // };
 // console.log(orderFood(cheque));
-
-// const user = { name: 'Коля', age: 9 };
+//
+// const userss = { name: 'Коля', age: 9 };
 // const diplom = { degree: 'Магистр', university: 'МГУ', department: 'Кафедра информационных технологий' };
-// console.log(mergeToNewObject(user, diplom));
-
-// const user = { name: 'Коля', age: 9, notes: 'Заметка от Коли!' };
+// console.log(mergeToNewObject(userss, diplom));
+//
+// const users = { name: 'Коля', age: 9, notes: 'Заметка от Коли!' };
 // const work = { address: 'Строительная 4', department: 'Шпаклёвщик', notes: 'Рабочая заметка' };
-// console.log(merge(user, work));
+// console.log(merge(users, work));
 //
 // console.log(report(['Александр', 'Михаил', 'Валентин', 'Сергей', 'Артур']));
-
+//
 // const ryan = {
 //   name: 'Райан',
 //   age: 28,
@@ -182,28 +208,109 @@ const color = new Date().toISOString();
 //   photo: 'https://photos.com/photo.jpg',
 // };
 // console.log(showInfo(ryan));
-
-// const object1 = { name: 'n', age: 10 };
-// const object2 = { age: 10, name: 'n' };
-
-// const object1 = { name: 'n', size: 3 };
-// const object2 = { size: 3 };
-//
-// const object1 = {};
-// const object2 = {};
-//
-// const object1 = { isAdult: true, email: 'example@mail.com', page: 100 };
-// const object2 = { page: 100, isAdult: true, email: 'example@mail.com' };
 //
 // const object1 = { checked: 1 };
 // const object2 = { checked: true };
-
+//
 // console.log(comparison(object1, object2));
-
+//
 // console.log(numbers());
 // console.log(averages());
 // console.log(double());
-const text = 'Это просто пример🙉, вы можете вписать абсолютно любую свою строку 😎 :)';
-console.log(revert(text));
+// const texts = 'Это просто пример🙉, вы можете вписать абсолютно любую свою строку 😎 :)';
+// console.log(revert(texts));
+//
+// const peoples = [
+//   { id: 8, name: 'Александр', gender: 'm', age: 25 },
+//   { id: 12, name: 'Мария', gender: 'f', age: 22 },
+//   { id: 31, name: 'Иван', gender: 'm', age: 17 },
+//   { id: 34, name: 'Ольга', gender: 'f', age: 19 },
+//   { id: 53, name: 'Дмитрий', gender: 'm', age: 30 },
+//   { id: 95, name: 'Екатерина', gender: 'f', age: 21 },
+//   { id: 3, name: 'Сергей', gender: 'm', age: 18 },
+//   { id: 20, name: 'Анна', gender: 'f', age: 20 },
+//   { id: 19, name: 'Андрей', gender: 'm', age: 15 },
+//   { id: 30, name: 'Наталья', gender: 'f', age: 25 },
+// ];
+// console.log(filter(peoples));
+//
+// console.log(rounding([1.1, 2.2, -2, 3.3, -1, 5.5, 5.99, 7.49, 7.9999, -2.2, -3]));
+// console.log(christmas(5));
+// console.log(colorNumber(generateInteger(5, 25)));
+//
+// const object111 = { isAdult: true, email: 'example@mail.com', page: 100 };
+// console.log(printColorObject(object111));
 
-//Подходящие люди
+// console.log(findMultiply(1234567890, 18)); //18
+// console.log(findMultiply(1234567890, 12)); //23
+// console.log(findMultiply(592729, 81)); //15
+// console.log(findMultiply(123, 5)); //-1-1
+// console.log(findMultiply(55, 25)); //01
+
+// const matr = [
+//   ['#', 'Имя', 'Возраст', 'Особые приметы'],
+//   [90, 'Паша', 23],
+//   [12, 'Артём', 29],
+//   [33, 'Стас', 13, 'Сидит на хлебной диете'],
+//   [54, 'Хабиб', 7],
+//   [35, 'Алеша', 15, 'Любит ночевать на лавочке'],
+//   [50, 'Лена', 19, 'Громко чихает'],
+//   [83, 'Катя', 43],
+//   [56, 'Алексей', 27, 'Мяукает во сне'],
+// ];
+//
+// console.log(matrix(matr));
+
+// const usersAge = [
+//   { name: 'Alice', age: 25 },
+//   { name: 'Bob', age: 30 },
+//   { name: 'Charlie', age: 25 },
+//   { name: 'Diana', age: 35 },
+//   { name: 'Eve', age: 30 },
+//   { name: 'Frank', age: 28 },
+//   { name: 'Grace', age: 25 },
+//   { name: 'Hannah', age: 41 },
+//   { name: 'Ivy', age: 30 },
+//   { name: 'Jack', age: 28 },
+// ];
+//
+// console.log(func(usersAge));
+
+// const userLength = {
+//   example: [1, 2, 3],
+//   'X-Address': {
+//     street: 'Ленина',
+//     house: '10-Б',
+//   },
+//   data: { length: 50 },
+//   nickname: null,
+//   permissions: [true, false],
+//   age: 101,
+//   emails: ['alice@example.com', 'bob@example.com', 'charlie@example.com', 'peter@example.com'],
+//   name: 'Peter Charles',
+//   isAdult: true,
+//   now: new Date().toISOString(),
+// };
+//
+// console.log(lengthArray(userLength));
+
+// console.log(compareArrays([], [])); // true
+// console.log(compareArrays([true, null, 1, 'stop'], [true, null, 1, 'stop'])); // true
+// console.log(compareArrays([1, 2, 3], [2, 1, 3])); // false
+// console.log(compareArrays([null], [])); // false
+//
+// console.log(compareObjects({}, {})); // true
+// console.log(compareObjects({ age: 45 }, { age: 45 })); // true
+// console.log(compareObjects({ ready: true, photo: null }, { ready: true, photo: null })); // true
+// console.log(compareObjects({ a: 10, b: 20, c: 30 }, { c: 30, a: 10, b: 20 })); // true
+// console.log(compareObjects({ name: 'max' }, { name: 'john' })); // false
+// console.log(compareObjects({ nick: 'maxon' }, {})); // false
+// console.log(compareObjects({ age: 45, name: 'a' }, { age: 45, name: 'b' })); // false
+
+console.log(
+  compareObjectArrays([{ age: 45, name: 'b' }, { name: 'john' }], [{ age: 45, name: 'b' }, { name: 'john' }]),
+);
+console.log(compareObjectArrays([{}], [{}, {}]));
+console.log(
+  compareObjectArrays([{ age: 45, name: 'b' }, { name: 'john' }], [{ age: 45, name: 'a' }, { name: 'john' }]),
+);
